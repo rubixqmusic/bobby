f42e00612c37e7d4c8e99f8ddaa28469ec0a8c56
John Ruiz <rubixqmusic@gmail.com>
2024-03-06 22:18:51 -0700

    added choice and play sound options to video call cutscene

--- res/framework/gamestates/videocallcutscenestates.py
+++ res/framework/gamestates/videocallcutscenestates.py
@@ -2,6 +2,7 @@ import pygame
 import logging
 import os
 import textwrap
+import math
 
 from res.settings import *
 from res.framework.state import State
@@ -17,6 +18,7 @@ VIDEO_CALL_ACCEPT_SOUND_PATH = f"{SOUNDS_PATH}/video_call_accept.wav"
 VIDEO_CALL_BLANK_BACKGROUND_PATH = f"{GRAPHICS_PATH}/backgrounds/video_call_blank_background.png"
 VIDEO_CALL_TEXT_BOX_PATH = f"{GRAPHICS_PATH}/backgrounds/video_call_text_box.png"
 
+DIALOG_SOUND_PATH = f"{SOUNDS_PATH}/dialog.wav"
 DIALOG_FONT_PATH = f"{FONTS_PATH}/{DEFAULT_FONT}"
 DIALOG_SIZE = 12
 DIALOG_COLOR = f"#ffffff"
@@ -180,9 +182,6 @@ class SetCharacterAnimation(State):
         video_call_cutscene.get_next_event()
 
 class ShowDialog(State):
-
-    
-
     def __init__(self, states: dict, *args) -> None:
         super().__init__(states, *args)
 
@@ -224,6 +223,7 @@ class ShowDialog(State):
                 new_text_surface = self.font.render(new_text,True,DIALOG_COLOR)
                 self.text_surfaces[self.current_line] = new_text_surface
                 self.current_character += 1
+                video_call_cutscene.game.play_sound(video_call_cutscene.game.load_resource(DIALOG_SOUND_PATH))
         
         elif self.status == "wait_for_button":
             if video_call_cutscene.game.is_button_released("start_button"):
@@ -235,16 +235,196 @@ class ShowDialog(State):
             for text_surface in self.text_surfaces:
                 video_call_cutscene.game.get_screen().blit(self.text_surfaces[text_surface], self.text_positions[text_surface])
 
-            ...
         
 
+class Wait(State):
+    def __init__(self, states: dict, *args) -> None:
+        super().__init__(states, *args)
+        self.args = args[0]
+        self.wait_time = self.args[0]
+        self.status = "waiting"
     
+    def update(self, video_call_cutscene):
+        if self.status == "waiting":
+            self.wait_time -= 1
+
+            if self.wait_time < 0:
+                self.status = "get_next_event"
+                video_call_cutscene.get_next_event()
+
 
 class EndCall(State):
     ...
 
 class Choice(State):
+    def __init__(self, states: dict, *args) -> None:
+        super().__init__(states, *args)
+
+        MAX_LINES = 2
+        TEXT_SIZE = 16
+        MAX_LINE_WIDTH = 32
+        self.args = args[0]
+        self.dialog = self.args[0]
+        self.choices_args = self.args[1]
+        self.choices = []
+        self.choice = ""
+        self.x_start = 124
+        self.x_spacing = 40
+        self.y_start = 196
+
+        self.max_text_grow = 5.0
+        self.text_grow_step_size = 0.1
+        self.sine_degrees = 0
+        self.grow_factor = 0
+
+        
+        self.text_surfaces = {}
+        self.text_lines = textwrap.wrap(self.dialog, MAX_LINE_WIDTH)
+        self.number_of_lines = len(self.text_lines)
+
+        self.text_positions = [
+                                [124, 178],
+                                [124, 196]
+        ]
+
+        self.current_character = 0
+        self.current_line = 0
+
+        self.status = "get_next_character"
+
+
+        for choice in self.choices_args:
+            new_choice = {}
+            new_choice[choice[0]] = choice[1]
+            new_choice["name"] = choice[0]
+            new_choice["text"] = choice[0]
+            self.choices.append(new_choice)
+
+            # self.choices[]
+    
+    def on_state_enter(self, video_call_cutscene):
+        self.font = pygame.font.Font(video_call_cutscene.game.load_resource(DIALOG_FONT_PATH), DIALOG_SIZE)
+    
+    def update(self, video_call_cutscene):
+        self.grow_factor = int(math.sin(self.sine_degrees) * self.max_text_grow)
+        self.sine_degrees += self.text_grow_step_size%self.max_text_grow
+
+        if self.status == "get_next_character":
+            if self.current_line > len(self.text_lines) -1:
+                self.current_line = 0
+                self.choice = self.choices[0]["name"]
+                self.status = "make_choice"
+            elif self.current_character > len(self.text_lines[self.current_line]):
+                self.current_character = 0
+                self.current_line += 1   
+            else:
+                new_text = self.text_lines[self.current_line][0:self.current_character]
+                new_text_surface = self.font.render(new_text,True,DIALOG_COLOR)
+                self.text_surfaces[self.current_line] = new_text_surface
+                self.current_character += 1
+                video_call_cutscene.game.play_sound(video_call_cutscene.game.load_resource(DIALOG_SOUND_PATH))
+        
+        elif self.status == "make_choice":
+            if video_call_cutscene.game.is_button_released("start_button"):
+                for choice in self.choices:
+                    if choice["name"] == self.choice:
+                        # video_call_cutscene.game.play_sound(video_call_cutscene.game.load_resource(VIDEO_CALL_ACCEPT_SOUND_PATH))
+                        next_event = choice[self.choice]
+                        states = video_call_cutscene.get_states_from_cutscene(next_event)
+                        video_call_cutscene.state = State(states)
+                        video_call_cutscene.event_index = -1
+                        video_call_cutscene.get_next_event()
+            elif video_call_cutscene.game.is_button_released("right_button"):
+                video_call_cutscene.game.play_sound(video_call_cutscene.game.load_resource(VIDEO_CALL_SELECT_SOUND_PATH))
+                self.choice = self.get_next_menu_item(self.choices, self.choice)
+            elif video_call_cutscene.game.is_button_released("left_button"):
+                video_call_cutscene.game.play_sound(video_call_cutscene.game.load_resource(VIDEO_CALL_SELECT_SOUND_PATH))
+                self.choice = self.get_previous_menu_item(self.choices, self.choice)
+
+    
+    def draw(self, video_call_cutscene):
+        if self.text_surfaces != {}:
+            for text_surface in self.text_surfaces:
+                video_call_cutscene.game.get_screen().blit(self.text_surfaces[text_surface], self.text_positions[text_surface])
+        if self.status == "make_choice":
+            self.draw_menu(self.choices,self.choice, self.x_start, self.x_spacing, self.font, DIALOG_COLOR, video_call_cutscene.game.get_screen(), self.grow_factor)
+    
+    def draw_menu(self,
+              menu: list, 
+              current_selection: str, 
+              x_start: int, 
+              x_spacing: int, 
+              font: pygame.font.Font, 
+              font_color: str, 
+              destination_surface: pygame.surface.Surface, 
+              grow_factor: int,
+              drop_shadow=False,
+              drop_shadow_color=f"#000000",
+              drop_shadow_x=1,
+              drop_shadow_y=1):
+    
+        menu_item_index = 0
+        for menu_item in menu:
+            text_surface = font.render(menu_item['text'],True,font_color)
+            text_surface_base_size = text_surface.get_width(), text_surface.get_height()
+            drop_shadow_surface = None
+            if drop_shadow:
+                drop_shadow_surface = font.render(menu_item['text'],False,drop_shadow_color)
+            
+            if menu_item["name"] == current_selection:
+                if drop_shadow_surface is not None:
+                    drop_shadow_surface_new = pygame.transform.scale(drop_shadow_surface, 
+                                        (text_surface_base_size[0] + grow_factor, 
+                                        text_surface_base_size[1] + grow_factor))
+                    drop_shadow_rect = drop_shadow_surface_new.get_rect(topleft=(drop_shadow_x, x_start + (menu_item_index*x_spacing)+drop_shadow_y, self.y_start))
+                    destination_surface.blit(drop_shadow_surface, drop_shadow_rect)
+                
+                new_surface = pygame.transform.scale(text_surface, 
+                                        (text_surface_base_size[0] + grow_factor, 
+                                        text_surface_base_size[1] + grow_factor))
+                text_rect = new_surface.get_rect(topleft=(x_start + (menu_item_index*x_spacing), self.y_start))
+                destination_surface.blit(new_surface, text_rect)
+            else:
+                if drop_shadow_surface is not None:
+                    drop_shadow_rect = drop_shadow_surface.get_rect(center=( x_start + (menu_item_index*x_spacing)+drop_shadow_y,self.y_start+drop_shadow_y))
+                    destination_surface.blit(drop_shadow_surface, drop_shadow_rect)
+                text_rect = text_surface.get_rect(topleft=(x_start + (menu_item_index*x_spacing), self.y_start))
+                destination_surface.blit(text_surface, text_rect)
+        
+            menu_item_index += 1
     ...
+
+    def get_next_menu_item(self, menu: list, current_selection: str):
+        index = 0
+        for menu_item in menu:
+            if menu_item["name"] == current_selection:
+                index += 1
+                if index > len(menu) - 1:
+                    index = 0
+                return menu[index]["name"]
+            index += 1
+
+    def get_previous_menu_item(self, menu: list, current_selection: str):
+        index = 0
+        for menu_item in menu:
+            if menu_item["name"] == current_selection:
+                index -= 1
+                if index < 0:
+                    index = len(menu) - 1
+                return menu[index]["name"]
+            index += 1
+        # return current_selection
+
+class PlaySound(State):
+    def __init__(self, states: dict, *args) -> None:
+        super().__init__(states, *args)
+        self.args = args[0]
+        self.sound_name = self.args[0]
+
+    def on_state_enter(self, video_call_cutscene):
+        video_call_cutscene.game.play_sound(video_call_cutscene.game.load_resource(f"{SOUNDS_PATH}/{self.sound_name}"))
+        video_call_cutscene.get_next_event()
+
 
 class GoTo(State):
     def __init__(self, states: dict, *args) -> None:
@@ -252,23 +432,17 @@ class GoTo(State):
         self.args = args[0]
         self.next_cutscene = self.args[0]
 
-
-
     def on_state_enter(self, video_call_cutscene):
-        # video_call_cutscene.go_to_next_event(self.next_cutscene)
         states = video_call_cutscene.get_states_from_cutscene(self.next_cutscene)
-        # print(states)
         video_call_cutscene.state = State(states)
         video_call_cutscene.event_index = -1
         video_call_cutscene.get_next_event()
-        # print(video_call_cutscene.state.states)
-        # video_call_cutscene.state.start(video_call_cutscene,0)
     
     def on_state_exit(self, video_call_cutscene):
         print(video_call_cutscene.states[video_call_cutscene.event_index])        
 
-        
+            
 
-        # video_call_cutscene.state.set_state(video_call_cutscene,video_call_cutscene.event_index)
-        
-    ...
+            # video_call_cutscene.state.set_state(video_call_cutscene,video_call_cutscene.event_index)
+            
+        ...
--- res/framework/videocallcutscenes.py
+++ res/framework/videocallcutscenes.py
@@ -1,4 +1,4 @@
-from res.framework.gamestates.videocallcutscenestates import VideoCallRinging, SetBackgroundImage, SetCharacterAnimation, Choice, ShowDialog, GoTo, EndCall
+from res.framework.gamestates.videocallcutscenestates import VideoCallRinging, SetBackgroundImage, PlaySound, Wait, SetCharacterAnimation, Choice, ShowDialog, GoTo, EndCall
 
 BLANK_BACKGROUND_PATH = f"backgrounds/video_call_blank_background.png"
 BEACH_BACKGROUND_PATH = f"backgrounds/video_call_beach_background.png"
@@ -10,43 +10,76 @@ BOBBY_OFFICE_ANIMATION_PATH = f"bobby_in_office.json"
 BOSS_VIDEO_CALL_SPRITESHEET_PATH = f"video_call_cutscenes/boss_video_call.png"
 BOSS_VIDEO_CALL_ANIMATION_PATH = f"boss_video_call.json"
 
+DENY_SOUND = f"deny.wav"
+COIN_SOUND = f"coin.wav"
+
 cutscenes = {
                 "intro_1" : [
                             (VideoCallRinging, {}),
                             (SetBackgroundImage, (1,BEACH_BACKGROUND_PATH, {})),
-                            (SetBackgroundImage, (2,OFFICE_BACKGROUND_PATH)),
+                            (SetBackgroundImage, (2,OFFICE_BACKGROUND_PATH, {})),
                             (SetCharacterAnimation, (1, BOSS_VIDEO_CALL_SPRITESHEET_PATH, BOSS_VIDEO_CALL_ANIMATION_PATH, "idle", {})),
                             (SetCharacterAnimation, (2, BOBBY_OFFICE_SPRITESHEET_PATH, BOBBY_OFFICE_ANIMATION_PATH, "idle", {})),
-                            (ShowDialog, ("How ya doin, Bobby?", {})),
+                            (Wait, (80, {})),
+                            (PlaySound, (DENY_SOUND, {})),
+                            (ShowDialog, ("Bobby, my man", {})),
+
+                            # (Choice, ("Got it?", [["Yes", "intro_end"], ["No", "intro_no"]], {})),
+
+                            (Wait, (60, {})),
+                            (ShowDialog, ("what's good, brother?", {})),
+                            (Wait, (60, {})),
+                            (ShowDialog, ("just wanted to hop on a call real quick", {})),
+                            (Wait, (60, {})),
+                            (ShowDialog, ("so we could connect on a couple of things", {})),
+                            (Wait, (80, {})),
+                            (ShowDialog, ("...okay... so...", {})),
+                            (Wait, (60, {})),
                             (ShowDialog, ("Listen... I'm not going to sugar coat it...", {})),
+                            (Wait, (30, {})),
                             (ShowDialog, ("The numbers are in, and...", {})),
+                            (Wait, (80, {})),
                             (ShowDialog, ("it's not looking good", {})),
+                            (Wait, (80, {})),
                             (ShowDialog, ("You're on thin fucking ice, Bobby", {})),
                             (ShowDialog, ("I NEED you to hit your fucking money quota", {})),
+                            (Wait, (80, {})),
                             (GoTo, ("intro_explanation", {}))
                             ],
       "intro_explanation" : [
                             (ShowDialog, ("okay... so...", {})),
+                            (Wait, (30, {})),
                             (ShowDialog, ("...here's what I'm thinking...", {})),
+                            (Wait, (80, {})),
                             (ShowDialog, ("We're giving you a new territory", {})),
                             (ShowDialog, ("and plenty of weapons", {})),
+                            (Wait, (80, {})),
                             (ShowDialog, ("it's very simple, Bobby", {})),
                             (ShowDialog, ("Do whatever it takes to hit your number", {})),
+                            (Wait, (80, {})),
                             (ShowDialog, ("or you're a dead motherfucker.", {})),
+                            (Wait, (60, {})),
                             (ShowDialog, ("Corporate wants us to get more rare stones,", {})),
                             (ShowDialog, ("so I want you to really focus on those.", {})),
+                            (Wait, (80, {})),
                             (ShowDialog, ("Oh, and one more thing...", {})),
+                            (Wait, (60, {})),
                             (ShowDialog, ("Try to make sure you don't, uh...", {})),
+                            (Wait, (60, {})),
                             (ShowDialog, ("injure too many bystanders", {})),
                             (ShowDialog, ("that gets us a lot of complaints from HR,", {})),
                             (ShowDialog, ("and thats bad", {})),
                             (Choice, ("Got it?", [["Yes", "intro_end"], ["No", "intro_no"]], {}))
                             ],
               "intro_no" :  [
+                            (PlaySound, (DENY_SOUND,{})),
+                            (Wait, (60, {})),
                             (ShowDialog, ("Goddammit, Bobby, I need you to pay attention", {})),
                             (GoTo, ("intro_explanation", {}))
                             ],
               "intro_end" : [
+                            (PlaySound, (COIN_SOUND,{})),
+                            (Wait, (80, {})),
                             (ShowDialog, ("Alright, Bobby", {})),
                             (ShowDialog, ("Get out there, and hit your fucking number", {})),
                             (EndCall,"level_select", {})
