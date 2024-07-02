273c27491b823399972961978b36c126727f1205
John Ruiz <rubixqmusic@gmail.com>
2024-07-02 13:41:17 -0700

    refactored file select screen

--- settings.py
+++ settings.py
@@ -98,7 +98,7 @@ OVERLAY_2_LAYER_NAME = "overlay_2"
 
 # Debug settings ------------------------------------------------------------]
 
-DEBUG_ENABLED = True
+DEBUG_ENABLED = False
 DEBUG_START_IN_STATE = "file_select_screen"
 DEBUG_LEVEL_NAME = "sunset_beach"
 DEBUG_LEVEL_START_POSITION = [185,466]
